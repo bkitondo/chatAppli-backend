fc39f3a41a3877ed8fb9314d285b084ec2344338
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-11-04 17:44:06 +0100

    feat: add feature socket

--- server.js
+++ server.js
@@ -4,29 +4,52 @@ require('./connexion/connexion')
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app.app)
-
-// const {Server} = require('socket.io')
-// const cors = require("cors")
-// const io = new Server(server, {
-//     cors : {
-//         origin:"http://localhost:3000",
-//         methods: ["GET","POST"]
-//     }
-// })
 const port = process.env.port || 8080
+const {Server} = require('socket.io')
+const cors = require("cors")
+const io = new Server(server, {
+  cors : {
+    origin:"http://localhost:3000",
+    methods: ["GET","POST"]
+  }
+})
+let users = []
+io.on("connection", (socket) => {
+  socket.on("add-user", (userId) => {
+    let socketId = socket.id
+    if (users.length < 1) {
+      users.push({
+        userId,
+        socketId 
+      }) 
+    }
+    else {
+      if(!users.some((user) => user.userId === userId)){
+        users.push({
+          userId,
+          socketId 
+        })
+      }
+      else {
+        users.map(user => {
+          if (user.userId === userId) {
+            user.socketId = socket.id
+          }
+        })
+      }
+    }
+    console.log("userId", userId , "socket.id", socket.id)
+  })     
 
-// io.on("connection", (socket)=>{
-//     console.log(`a user connected ${socket.id}`);
-//     socket.on("send_message", (data)=>{
-//         socket.emit("receive_message", data)
-//         console.log(data);
-//     })
-// })
-
+  console.log("users", users )
+  socket.on("send-msg", (data) => {
+    console.log("voici le data", data)
+    console.log("users send ", users)
+    const user = users.find(user => user.userId === data.receiver)
+    console.log("user socket reussi", user )
+    if(user){
+      io.to(user.socketId).emit("msg-received",data)
+    }
+  })
+})
 server.listen(port, console.log(`server is starting port ${port}`))
-
-
-
-
-
-
